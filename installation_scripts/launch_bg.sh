cfd92626feec76906a4d895ef2f0cb94308010e7
thomas bush <thomasbush52@gmail.com>
2026-01-26 09:31:13 -0500

    script to run bg with better control over gpu usage

--- installation_scripts/launch_bg.sh
+++ installation_scripts/launch_bg.sh
@@ -5,12 +5,9 @@ set -x
 
 INPUT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/examples/gfp_design.yaml"
 OUTPUT_DIR="/n/home06/tbush/outputs/boltz_gen/"
-BOLTZ_GEN_SCRIPT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/bg_1st_run.slrm"
+BOLTZ_GEN_SCRIPT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/bg_run.slrm"
 NUM_DESIGNS=${1:-1000}
 mkdir -p "${OUTPUT_DIR}"
 
-
-
 echo "Launch Boltz Generation"
 sbatch "${BOLTZ_GEN_SCRIPT}" "${INPUT}" "${OUTPUT_DIR}" "${NUM_DESIGNS}"
-
