8a80490306f882106fc728e4bf0c38ae87f41a7a
Thomasbush9 <thomasbush52@gmail.com>
2026-01-26 11:42:28 -0500

    minor change

--- installation_scripts/launch_bg.sh
+++ installation_scripts/launch_bg.sh
@@ -5,8 +5,8 @@ set -x
 
 INPUT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/examples/gfp_design.yaml"
 OUTPUT_DIR="/n/home06/tbush/outputs/boltz_gen/"
-BOLTZ_GEN_SCRIPT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/bg_run.slrm"
-NUM_DESIGNS=${1:-1000}
+BOLTZ_GEN_SCRIPT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/bg_1st_run.slrm"
+NUM_DESIGNS=${1:-50}
 mkdir -p "${OUTPUT_DIR}"
 
 echo "Launch Boltz Generation"
