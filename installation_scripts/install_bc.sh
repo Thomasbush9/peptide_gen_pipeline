a239c789405f15fdb8daaf1c19861f7a7059abca
Thomasbush9 <thomasbush52@gmail.com>
2026-01-27 13:37:23 -0500

    script to install bindcraft

--- installation_scripts/install_bc.sh
+++ installation_scripts/install_bc.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 INSTALL_FOLDER="${1:-}"
 CUDA_VERSION=12.9
-PKG_MANAGER="mamba"
+PKG_MANAGER="conda"
 if [[ -z "${INSTALL_FOLDER}" ]]; then
   echo "You must specify an install folder to install BindCraft"
   exit 1
@@ -16,8 +16,10 @@ cd "${INSTALL_FOLDER}"
 module purge
 
 module load python/3.12.8-fasrc01 gcc/14.2.0-fasrc01 cuda/12.9.1-fasrc01 cudnn/9.10.2.21_cuda12-fasrc01
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
 echo "Creating bindcraft Env"
-mamba create -n bindcraft python=3.12.8
-mamba activate bindcraft
+conda create -n bindcraft python=3.12.8
+conda activate bindcraft
 echo "Installing bindcraft in ${INSTALL_FOLDER}"
 bash install_bindcraft.sh --cuda "${CUDA_VERSION}" --pkg_manager "${PKG_MANAGER}"
--- installation_scripts/launch_bg.sh
+++ installation_scripts/launch_bg.sh
@@ -6,7 +6,7 @@ set -x
 INPUT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/examples/gfp_design.yaml"
 OUTPUT_DIR="/n/home06/tbush/outputs/boltz_gen/"
 BOLTZ_GEN_SCRIPT="/n/home06/tbush/peptide_gen_pipeline/installation_scripts/bg_1st_run.slrm"
-NUM_DESIGNS=${1:-50}
+NUM_DESIGNS=${1:-500}
 mkdir -p "${OUTPUT_DIR}"
 
 echo "Launch Boltz Generation"
