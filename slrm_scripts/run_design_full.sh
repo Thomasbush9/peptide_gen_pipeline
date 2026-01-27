06e3400f8bd751c0bbfb95769db9d1046bebf644
thomas bush <thomasbush52@gmail.com>
2026-01-27 18:15:05 -0500

    config files

--- slrm_scripts/run_design_full.sh
+++ slrm_scripts/run_design_full.sh
@@ -11,10 +11,19 @@ BG_OUTPUT_FOLDER="${OUTPUT_FOLDER}/bg/"
 BC_OUTPUT_FOLDER="${OUTPUT_FOLDER}/bc/"
 
 BG_INPUT_FILE="${INPUT_FOLDER}/design_config.yaml"
-BC_INPUT_FILE="${INPUT_FOLDER}/design_config.json"
+BC_INPUT_FILE="${INPUT_FOLDER}"
+BC_SETTINGS_TARGET="${INPUT_FOLDER}/settings_target/target_settings.json"
+BC_SETTINGS_FILTER="${INPUT_FOLDER}/settings_filters/filter_settings.json"
+BC_SETTINGS_ADV="${INPUT_FOLDER}/settings_adv/adv_settings.json"
 
 mkdir -p "${BG_OUTPUT_FOLDER}"
 mkdir -p "${BC_OUTPUT_FOLDER}"
 
 echo "Launching Boltz Gen Predictions:"
 sbatch "${BG_SLURM_SCRIPT}" "${BG_INPUT_FILE}" "${BG_OUTPUT_FOLDER}" "{$NUM_DESIGNS}"
+
+echo "Launching BindCraft Predictions: "
+sbatch "${BC_SLURM_SCRIPT}" \
+  --settings "${BC_SETTINGS_TARGET}" \ 
+# --filters "${BC_SETTINGS_FILTER}" \
+# --advanced "${BC_SETTINGS_ADV}"
